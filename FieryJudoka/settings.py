ef01b7849dbd58e32038aa359e85816acc895f45
Raython123 <90645063+Raython123@users.noreply.github.com>
2024-07-09 23:25:55 +0000

    Update settings.py

--- FieryJudoka/settings.py
+++ FieryJudoka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2)mguwl6=1qb@xy=g_msoeivin&dna0(%3%rsuq_=ii60#e1!e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 AUTH_USER_MODEL = 'user.User'
 

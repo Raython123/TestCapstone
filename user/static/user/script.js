78155c02fd30c8b90167c78b94822ce4b6de8311
Raython123 <rayane.at08@gmail.com>
2024-07-10 23:25:49 +0000

    New changes

--- user/static/user/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let userbelt = document.querySelector('#userbelt');
-    for (let i = 0; i < userbelt.options.length; i++) {
-        let option = userbelt.options[i];
-        if (option.value.includes(userbelt.value)) {
-            option.selected = true;
-        }
-    }
-});
